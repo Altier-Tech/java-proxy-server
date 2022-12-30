de0e86b42a1a104ed8b6334d3b5d01c465c3542b
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 09:58:41 +0530

    Update PropertiesHandler.java

--- src/main/java/tech/altier/AppProperties/PropertiesHandler.java
+++ src/main/java/tech/altier/AppProperties/PropertiesHandler.java
@@ -1,4 +1,22 @@
 package tech.altier.AppProperties;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class PropertiesHandler {
+    public PropertiesHandler(String propertyName) {
+    }
+
+    private static Properties loadProperties() throws IOException {
+        log("Loading application properties...");
+        Properties configuration = new Properties();
+        InputStream inputStream = PropertiesLoader.class
+                .getClassLoader()
+                .getResourceAsStream("application.properties");
+        configuration.load(inputStream);
+        assert inputStream != null;
+        inputStream.close();
+        return configuration;
+    }
 }
