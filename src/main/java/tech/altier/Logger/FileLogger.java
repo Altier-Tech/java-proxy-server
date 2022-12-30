bb0d5a4c1f1857dce27e2351a0979c76436bbfb1
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 15:29:58 +0530

    Update FileLogger.java

--- src/main/java/tech/altier/Logger/FileLogger.java
+++ src/main/java/tech/altier/Logger/FileLogger.java
@@ -3,17 +3,20 @@ package tech.altier.Logger;
 class FileLogger implements Logger {
     @Override
     public void log(String message) {
-        System.out.println("FileLogger: " + message);
+        write("FileLogger: " + message);
     }
 
     @Override
     public void logln(String message) {
-        System.out.println("FileLogger: " + message);
+        write("FileLogger: " + message + "/n");
     }
 
     @Override
     public void error(String message) {
-        System.out.println("FileLogger: " + message);
+        write("FileLogger: " + message);
     }
-}
+
+    private void write(String message) {
+        // Write to file
+    }
 }
