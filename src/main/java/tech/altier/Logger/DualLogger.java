b69dbbefc54f0eabba6980d2ce925ce96744983e
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 15:39:00 +0530

    Update DualLogger.java

--- src/main/java/tech/altier/Logger/DualLogger.java
+++ src/main/java/tech/altier/Logger/DualLogger.java
@@ -1,4 +1,29 @@
 package tech.altier.Logger;
 
-public class DualLogger implements Logger {
+class DualLogger implements Logger {
+    private final Logger consoleLogger;
+    private final Logger fileLogger;
+
+    public DualLogger() {
+        consoleLogger = new ConsoleLogger();
+        fileLogger = new FileLogger();
+    }
+
+    @Override
+    public void log(String message) {
+        consoleLogger.log(message);
+        fileLogger.log(message);
+    }
+
+    @Override
+    public void logln(String message) {
+        consoleLogger.logln(message);
+        fileLogger.logln(message);
+    }
+
+    @Override
+    public void error(String message) {
+        consoleLogger.error(message);
+        fileLogger.error(message);
+    }
 }
