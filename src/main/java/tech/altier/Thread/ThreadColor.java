3d5d5fd908f9949e7a26e88584668119062880b6
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 15:45:32 +0530

    Update ThreadColor.java

--- src/main/java/tech/altier/Thread/ThreadColor.java
+++ src/main/java/tech/altier/Thread/ThreadColor.java
@@ -14,8 +14,8 @@ public class ThreadColor {
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_LIGHT_YELLOW = "\u001B[93m";
     public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_LIGHT_YELLOW = "\u001B[93m";
     public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
     public static final String ANSI_WHITE = "\u001B[37m";
     public static final String ANSI_BOLD = "\u001B[1m";
