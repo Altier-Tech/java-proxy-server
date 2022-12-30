b00eeca5f30ee9b9de957587fdb45e5dff3b8729
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 17:21:01 +0530

    Update RequestHandler.java

--- src/main/java/tech/altier/JProxy/core/RequestHandler.java
+++ src/main/java/tech/altier/JProxy/core/RequestHandler.java
@@ -2,19 +2,31 @@ package tech.altier.JProxy.core;
 
 import tech.altier.JProxy.Main;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 
 public class RequestHandler implements Runnable {
-    private final int port;
     private ServerSocket serverSocket;
 
     public RequestHandler(int port) {
-        this.port = port;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            Main.logger.logln("Error creating server socket: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void run() {
         Main.logger.logln("Listening started on port " + port);
 
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                serverSocket.accept();
+            } catch (Exception e) {
+                Main.logger.logln("Error while listening: " + e.getMessage());
+            }
+        }
     }
 }
