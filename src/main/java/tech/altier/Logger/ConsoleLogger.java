8f42187c54ed3c94a807f97867781bbf1c2fd963
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 15:16:06 +0530

    Update ConsoleLogger.java

--- src/main/java/tech/altier/Logger/ConsoleLogger.java
+++ src/main/java/tech/altier/Logger/ConsoleLogger.java
@@ -2,4 +2,48 @@ package tech.altier.Logger;
 
 public class ConsoleLogger implements Logger {
 
+    @Override
+    public void log(String message) {
+        System.out.print(message);
+    }
+
+    @Override
+    public void log(String message, String threadName) {
+
+    }
+
+    @Override
+    public void log(String message, String threadName, String className) {
+
+    }
+
+    @Override
+    public void logln(String message) {
+
+    }
+
+    @Override
+    public void logln(String message, String threadName) {
+
+    }
+
+    @Override
+    public void logln(String message, String threadName, String className) {
+
+    }
+
+    @Override
+    public void error(String message) {
+
+    }
+
+    @Override
+    public void error(String message, String threadName) {
+
+    }
+
+    @Override
+    public void error(String message, String threadName, String className) {
+
+    }
 }
