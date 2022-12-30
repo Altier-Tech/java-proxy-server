eb2e96f6ee393849694439f8372a99b621a81fae
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 17:10:02 +0530

    Update ProxyServer.java

--- src/main/java/tech/altier/JProxy/core/ProxyServer.java
+++ src/main/java/tech/altier/JProxy/core/ProxyServer.java
@@ -1,4 +1,27 @@
 package tech.altier.JProxy.core;
 
 public class ProxyServer {
+    private static ProxyServer instance;
+    private int port;
+
+    private ProxyServer(int port) {
+        this.port = port;
+    }
+
+    private ProxyServer() {
+        this(8080);
+    }
+
+    public static ProxyServer getInstance() {
+        if (instance == null) instance = new ProxyServer();
+        return instance;
+    }
+
+    public void start() {
+        System.out.println("Starting proxy server...");
+    }
+
+    public void stop() {
+        System.out.println("Stopping proxy server...");
+    }
 }
