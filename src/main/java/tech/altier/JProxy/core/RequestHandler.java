09f51603063e8107fa55ca4954e51c9792e4246e
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 17:16:28 +0530

    Update RequestHandler.java

--- src/main/java/tech/altier/JProxy/core/RequestHandler.java
+++ src/main/java/tech/altier/JProxy/core/RequestHandler.java
@@ -1,6 +1,12 @@
 package tech.altier.JProxy.core;
 
 public class RequestHandler implements Runnable {
+    private int port;
+
+    public RequestHandler(int port) {
+        this.port = port;
+    }
+    
     @Override
     public void run() {
 
