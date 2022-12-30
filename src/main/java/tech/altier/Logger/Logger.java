b51072c4cedd67143d36ec81ef8504e96a89a5c1
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 15:14:08 +0530

    Update Logger.java

--- src/main/java/tech/altier/Logger/Logger.java
+++ src/main/java/tech/altier/Logger/Logger.java
@@ -2,4 +2,10 @@ package tech.altier.Logger;
 
 public interface Logger {
     void log(String message);
+    void log(String message, String threadName);
+    void log(String message, String threadName, String className);
+    void error(String message);
+    void error(String message, String threadName);
+    void error(String message, String threadName, String className);
+    
 }
