2eff513ce94cac9370ffc2825a7fdfbcad13d836
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 15:23:41 +0530

    Update ConsoleLogger.java

--- src/main/java/tech/altier/Logger/ConsoleLogger.java
+++ src/main/java/tech/altier/Logger/ConsoleLogger.java
@@ -5,15 +5,18 @@ import tech.altier.Thread.ThreadColor;
 public class ConsoleLogger implements Logger {
     @Override
     public void log(String message) {
-        System.out.print(Thread.currentThread().getName() + "\t" + message);
+        System.out.print(
+                LogManager.getColor() + Thread.currentThread().getName() + "\t" + message);
     }
 
     @Override
     public void logln(String message) {
         System.out.println(
+                LogManager.getColor() +
                 Thread.currentThread().getName() +
                 "\t" +
-                message);
+                message
+        );
     }
 
     @Override
@@ -22,6 +25,7 @@ public class ConsoleLogger implements Logger {
                 ThreadColor.ANSI_RED +
                 Thread.currentThread().getName() +
                 "\t" +
-                message);
+                message
+        );
     }
 }
