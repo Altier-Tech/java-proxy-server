bc2f1c72950e0c587e25efd0b43ee3e553d8e51d
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 10:22:07 +0530

    Update Main.java

--- src/main/java/tech/altier/ProxyServer/Main.java
+++ src/main/java/tech/altier/ProxyServer/Main.java
@@ -5,11 +5,14 @@ import tech.altier.AppProperties.PropertiesHandler;
 import java.io.IOException;
 
 public class Main {
+    public static PropertiesHandler applicationProperties;
+
     public static void main(String[] args) {
         try {
-            PropertiesHandler applicationProperties = new PropertiesHandler("application").loadProperties();
+            applicationProperties = new PropertiesHandler("application").loadProperties();
         } catch (IOException e) {
-            log
+            log("Error loading application properties: " + e.getMessage());
+            // Create a new properties file
         }
     }
 }
