0cb43f434f4e9928f7363fb398976dc7f4398496
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 17:02:00 +0530

    Update Server.java

--- src/main/java/tech/altier/JProxy/core/Server.java
+++ src/main/java/tech/altier/JProxy/core/Server.java
@@ -1,4 +1,13 @@
 package tech.altier.JProxy.core;
 
 public class Server {
+    private int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public Server start() {
+        return this;
+    }
 }
