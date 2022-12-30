94a1461f3408fc186341753c1ceec4398742310d
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 16:25:39 +0530

    Update Main.java

--- src/main/java/tech/altier/JProxy/Main.java
+++ src/main/java/tech/altier/JProxy/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
         // Init
 //        loadApplicationProperties();
-        logger = new LogManager(LoggerType.FILE);
+        logger = new LogManager(LoggerType.BOTH);
 
         // Start the proxy server
         logger.logln("Starting proxy server...");
