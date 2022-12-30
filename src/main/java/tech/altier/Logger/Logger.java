47f66a9fbc3b6cdd16530047e4702e5f89fe861f
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 15:15:49 +0530

    Update Logger.java

--- src/main/java/tech/altier/Logger/Logger.java
+++ src/main/java/tech/altier/Logger/Logger.java
@@ -4,6 +4,11 @@ public interface Logger {
     void log(String message);
     void log(String message, String threadName);
     void log(String message, String threadName, String className);
+
+    void logln(String message);
+    void logln(String message, String threadName);
+    void logln(String message, String threadName, String className);
+
     void error(String message);
     void error(String message, String threadName);
     void error(String message, String threadName, String className);
