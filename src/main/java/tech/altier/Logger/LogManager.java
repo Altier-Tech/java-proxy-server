5ec8e00205a697fc64cf1f4e8cdb6734fe639eeb
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-30 15:12:36 +0530

    Update LogManager.java

--- src/main/java/tech/altier/Logger/LogManager.java
+++ src/main/java/tech/altier/Logger/LogManager.java
@@ -1,8 +1,20 @@
 package tech.altier.Logger;
 
 public class LogManager {
-    public LogManager(LoggerType type) {
+    private static Logger logger;
 
+    public LogManager(LoggerType type) {
+        switch (type) {
+            case CONSOLE:
+                logger = new ConsoleLogger();
+                break;
+            case FILE:
+                logger = new FileLogger();
+                break;
+            case BOTH:
+                logger = new DualLogger();
+                break;
+        }
     }
 
     public LogManager() {
