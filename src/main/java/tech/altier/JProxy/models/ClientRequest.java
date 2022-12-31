f2ed36bc7d69026c0e9838fd41fcb1170e0d3d13
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-31 13:09:23 +0530

    Update ClientRequest.java

--- src/main/java/tech/altier/JProxy/models/ClientRequest.java
+++ src/main/java/tech/altier/JProxy/models/ClientRequest.java
@@ -3,6 +3,10 @@ package tech.altier.JProxy.models;
 import tech.altier.JProxy.Main;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.SocketException;
 
 public class ClientRequest {
     HttpMethod method;
@@ -36,4 +40,33 @@ public class ClientRequest {
         );
     }
 
+    public static String getClientRequest(Socket client) throws IOException, SocketException {
+        System.out.println("Debug: got new client " + client.toString());
+        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream(), "UTF-8"));
+
+        StringBuilder requestBuilder = new StringBuilder();
+        String line;
+        int contectLegnth = 0;
+
+        while (!(line = br.readLine()).isBlank()) {
+            requestBuilder.append(line + "\r\n");
+            if (line.toLowerCase().startsWith("content-length")) {
+                contectLegnth = Integer.parseInt(line.split(":")[1].trim());
+            }
+        }
+
+
+        StringBuilder requestBodyBuilder = new StringBuilder();
+        if (contectLegnth > 0) {
+            int read;
+            while ((read = br.read()) != -1) {
+                requestBodyBuilder.append((char) read);
+                if (requestBodyBuilder.length() == contectLegnth)
+                    break;
+            }
+            requestBuilder.append("\r\n" + requestBodyBuilder);
+        }
+
+        return requestBuilder.toString();
+    }
 }
