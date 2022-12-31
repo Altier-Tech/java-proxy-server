54d9d342714998bc5b5badd69ec374fe24a68d7a
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-31 15:04:51 +0530

    Update Response.java

--- src/main/java/tech/altier/JProxy/http/Response.java
+++ src/main/java/tech/altier/JProxy/http/Response.java
@@ -1,4 +1,19 @@
 package tech.altier.JProxy.http;
 
+import java.net.http.HttpResponse;
+
 public class Response {
+    private final String version;
+    private final int statusCode;
+    private final String body;
+
+    private Response(String version, int statusCode, String body) {
+        this.version = version;
+        this.statusCode = statusCode;
+        this.body = body;
+    }
+
+    public Response(HttpResponse<String> responseFromServer) {
+        this(responseFromServer.version().toString(), responseFromServer.statusCode(), responseFromServer.body());
+    }
 }
