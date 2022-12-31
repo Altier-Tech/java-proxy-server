a38129eb1d97ade8c5e4e75800484f5b84097531
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-31 14:39:01 +0530

    Update JacksonOM.java

--- src/main/java/tech/altier/JSONParse/JacksonOM.java
+++ src/main/java/tech/altier/JSONParse/JacksonOM.java
@@ -2,11 +2,17 @@ package tech.altier.JSONParse;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import tech.altier.JProxy.http.GETRequest;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class JacksonOM {
+    public static GETRequest parseGETRequest(String inputString) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(inputString, GETRequest.class);
+    }
+
     public static Map<String, String> mapStringToJSON(String input) throws IOException {
         return new ObjectMapper().readValue(input.getBytes(), new TypeReference<Map<String,String>>(){});
     }
