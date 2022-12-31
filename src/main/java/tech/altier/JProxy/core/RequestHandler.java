2c76860a4ed740ac5a82612b0e3e11da39224351
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-31 15:10:48 +0530

    Update RequestHandler.java

--- src/main/java/tech/altier/JProxy/core/RequestHandler.java
+++ src/main/java/tech/altier/JProxy/core/RequestHandler.java
@@ -37,8 +37,6 @@ public class RequestHandler implements Runnable {
 
             HttpResponse<String> responseFromServer = null;
 
-            System.out.println(requestFromClient.getMethod() + " " + requestFromClient.getEndpoint());
-
             // If the request is GET
             if (requestFromClient.getMethod() == HttpMethod.GET) {
                 responseFromServer = (new GETRequest(
