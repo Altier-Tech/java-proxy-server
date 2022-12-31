a280b5e2cb2da0cfc4cd680a0f490fb8324941e9
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-31 14:32:11 +0530

    Update JacksonOM.java

--- src/main/java/tech/altier/JSONParse/JacksonOM.java
+++ src/main/java/tech/altier/JSONParse/JacksonOM.java
@@ -1,30 +1,15 @@
 package tech.altier.JSONParse;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Iterator;
+import java.io.IOException;
+import java.util.Map;
 
 public class JacksonOM {
-    public static void mapJSON(String input) {
-        //read json file data to String
-        byte[] jsonData =input.getBytes();
-
-        //create ObjectMapper instance
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        //read JSON like DOM Parser
-        JsonNode rootNode = objectMapper.readTree(jsonData);
-        JsonNode idNode = rootNode.path("id");
-        System.out.println("id = "+idNode.asInt());
-
-        JsonNode phoneNosNode = rootNode.path("phoneNumbers");
-        Iterator<JsonNode> elements = phoneNosNode.elements();
-        while(elements.hasNext()){
-            JsonNode phone = elements.next();
-            System.out.println("Phone No = "+phone.asLong());
-        }
+    public static void mapJSON(String input) throws IOException {
+        byte[] mapData = input.getBytes();
+        Map<String,String> myMap = new ObjectMapper().readValue(mapData, new TypeReference<Map<String,String>>(){});
+        System.out.println("Map using TypeReference: " + myMap);
     }
 }
