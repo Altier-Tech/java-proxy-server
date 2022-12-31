4026060b3bb81a060ab23f0ba4edeb39a136d2b0
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-31 10:14:54 +0530

    Update GETRequest.java

--- src/main/java/tech/altier/JProxy/http/GETRequest.java
+++ src/main/java/tech/altier/JProxy/http/GETRequest.java
@@ -7,7 +7,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class GETRequest {
-    String endPoint;
+    private String endPoint;
 
     public GETRequest(String endPoint) {
         this.endPoint = endPoint;
