de6dc14dd67f7c6fba9fd1e10f6c30bb4d2972e9
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-31 14:36:01 +0530

    Update JacksonOM.java

--- src/main/java/tech/altier/JSONParse/JacksonOM.java
+++ src/main/java/tech/altier/JSONParse/JacksonOM.java
@@ -10,6 +10,8 @@ public class JacksonOM {
     public static void mapJSON(String input) throws IOException {
         byte[] mapData = input.getBytes();
         Map<String,String> myMap = new ObjectMapper().readValue(mapData, new TypeReference<Map<String,String>>(){});
-        System.out.println("Map using TypeReference: " + myMap);
+        System.out.println(
+                myMap.get("hello")
+        );
     }
 }
