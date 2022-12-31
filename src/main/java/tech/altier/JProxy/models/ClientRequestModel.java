c52da4aa39ee72029a56cd932764413c15b4002d
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-31 09:44:12 +0530

    Update ClientRequestModel.java

--- src/main/java/tech/altier/JProxy/models/ClientRequestModel.java
+++ src/main/java/tech/altier/JProxy/models/ClientRequestModel.java
@@ -2,5 +2,14 @@ package tech.altier.JProxy.models;
 
 public class ClientRequestModel {
     HttpMethod method;
+    String endpoint;
+    String version;
+
+    public ClientRequestModel(HttpMethod method, String endpoint, String version) {
+        this.method = method;
+        this.endpoint = endpoint;
+        this.version = version;
+    }
+
     
 }
